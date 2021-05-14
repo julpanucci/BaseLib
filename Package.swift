500822fd7b8102b9162e49288cacab41fe232046
Julian Panucci <julian.panucci@underarmour.com>
2021-05-14 16:00:36 -0500

    Adding xcframework

--- Package.swift
+++ Package.swift
@@ -9,7 +9,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BaseLib",
-            targets: ["BaseLib"]),
+            targets: ["BaseLib"]
+		),
+		.library(name: "BaseLibFramework",
+				 targets: ["BaseLibFramework"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +21,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+		
+		.binaryTarget(name: "BaseLibFramework", path: "./Baselib.xcframework"),
         .target(
             name: "BaseLib",
             dependencies: []),
